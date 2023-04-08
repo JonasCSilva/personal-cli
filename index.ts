b8db582159a5cffdca4bc3d98b2b06f653a39486
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2023-04-08 15:25:53 -0300

    feat: create checkPath function

--- index.ts
+++ index.ts
@@ -1,7 +1,31 @@
-const home = Deno.env.get('USERPROFILE')!
+import { join } from 'https://deno.land/std@0.182.0/path/mod.ts'
+import { ignoreFiles, ignorePaths } from './ignore.ts'
 
-const dir = Deno.readDir(home)
+const basePath = Deno.env.get('USERPROFILE')!
 
-for await (const dirEntry of dir) {
-  console.log(dirEntry.name)
+const isIgnoredPath = (entryPath: string) => {
+  for (const ignorePath of ignorePaths) {
+    if (join(basePath, ignorePath) === entryPath) {
+      return true
+    }
+  }
+  return false
+}
+
+const isIgnoredFile = (entryName: string) => ignoreFiles.includes(entryName)
+
+checkPath(basePath)
+
+async function checkPath(path: string) {
+  const dir = Deno.readDir(path)
+
+  for await (const entry of dir) {
+    const entryPath = join(path, entry.name)
+
+    if (entry.isSymlink) continue
+    if (isIgnoredPath(join(path, entry.name))) continue
+    if (isIgnoredFile(entry.name)) continue
+    if (entry.isFile) console.log(entryPath)
+    if (entry.isDirectory) checkPath(entryPath)
+  }
 }
