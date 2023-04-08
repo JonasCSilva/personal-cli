6fbb01c11ca9d17f8dfc9b797b927a0736a42804
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2023-04-08 17:45:08 -0300

    feat: add cliffy

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
 import { checkPath } from './functions.ts'
 
-const basePath = Deno.env.get('USERPROFILE')!
-
-const results: string[] = []
-
-await checkPath(results, basePath)
-
-console.log()
-
-if (results.length > 0) {
-  for (const result of results) {
-    console.log(result)
+export async function main() {
+  const basePath = Deno.env.get('USERPROFILE')!
+  
+  const results: string[] = []
+  
+  await checkPath(results, basePath)
+  
+  console.log()
+  
+  if (results.length > 0) {
+    for (const result of results) {
+      console.log(result)
+    }
+  } else {
+    console.log('No files found!')
   }
-} else {
-  console.log('No files found!')
+  
+  console.log()
 }
-
-console.log()
