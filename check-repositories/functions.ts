0f83dd090fb3990ea1df2823e7e7450f1aeef92e
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2023-04-08 22:23:27 -0300

    feat: colorize cr output

---
+++ check-repositories/functions.ts
@@ -0,0 +1,52 @@
+import { join } from 'https://deno.land/std@0.181.0/path/mod.ts'
+import { bold, red } from 'https://deno.land/std@0.182.0/fmt/colors.ts'
+
+export type GetStrings = (repositoriesPath: string, dir: AsyncIterable<Deno.DirEntry>) => Promise<string[]>
+
+export const getStrings: GetStrings = async (repositoriesPath, dir) => {
+  const promises = []
+
+  for await (const dirEntry of dir) {
+    const p = Deno.run({
+      cmd: ['git', '-C', join(repositoriesPath, dirEntry.name), 'fetch', '--all'],
+    })
+
+    const promise = p.status().then(async () => {
+      const cmd = Deno.run({
+        cmd: ['git', '-C', join(repositoriesPath, dirEntry.name), 'status', '-b', '--porcelain'],
+        stdout: 'piped',
+        stderr: 'piped',
+      })
+
+      const output = await cmd.output()
+      const outStr = new TextDecoder().decode(output)
+
+      const lines = outStr.split('\n').filter((_) => _)
+
+      return getString(dirEntry.name, lines)
+    })
+
+    promises.push(promise)
+  }
+
+  return Promise.all(promises)
+}
+
+function getString(entryName: string, lines: string[]) {
+  let string = bold(`${entryName}:`)
+
+  for (const [index, line] of lines.entries()) {
+    if (index === 0) {
+      const indexOf = line.indexOf('[')
+      if (indexOf === -1) {
+        string += `\n  ${line}`
+      } else {
+        string += `\n  ${line.substring(0, indexOf)}${red(line.substring(indexOf))}`
+      }
+    } else {
+      string += red(`\n  ${line}`)
+    }
+  }
+
+  return `\n${string}\n`
+}
--- check-repositories/index.ts
+++ check-repositories/index.ts
@@ -1,30 +1,19 @@
 import { join } from 'https://deno.land/std@0.181.0/path/mod.ts'
-import { fn } from './fn1.ts'
+import { getStrings } from './functions.ts'
 
 export async function checkRepositories(argPath?: string) {
   const home = Deno.env.get('USERPROFILE')!
 
   argPath ??= 'repositories'
 
-  if (typeof argPath !== 'string') {
-    console.log(
-      '\n%cSpecify a path relative to the home folder!\n',
-      'font-weight: bold; color: red',
-    )
-
-    Deno.exit()
-  }
-
   const repositoriesPath = join(home, argPath)
 
   try {
     const dir = Deno.readDir(repositoriesPath)
 
-    const strings = await fn(repositoriesPath, dir)
+    const strings = await getStrings(repositoriesPath, dir)
 
-    for (const string of strings) {
-      console.log(string, 'font-weight: bold', 'font-weight: normal')
-    }
+    console.log(...strings)
   } catch (error) {
     if (!(error instanceof Deno.errors.NotFound)) {
       throw error
@@ -34,7 +23,5 @@ export async function checkRepositories(argPath?: string) {
       '\n%cFolder not found!\n',
       'font-weight: bold; color: red',
     )
-
-    Deno.exit()
   }
 }
