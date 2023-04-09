0f9d3b91b1f2b131e5ef1d9a536e3c20453680e0
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2023-04-08 21:34:18 -0300

    feat: colorize cf output

--- check-folders/index.ts
+++ check-folders/index.ts
@@ -1,21 +1,22 @@
 import { checkPath } from './functions.ts'
+import { green, red } from 'https://deno.land/std@0.182.0/fmt/colors.ts'
 
 export async function checkFolders() {
   const basePath = Deno.env.get('USERPROFILE')!
-  
+
   const results: string[] = []
-  
+
   await checkPath(results, basePath)
-  
+
   console.log()
-  
+
   if (results.length > 0) {
     for (const result of results) {
-      console.log(result)
+      console.log(red(result))
     }
   } else {
-    console.log('No files found!')
+    console.log(green('No files found!'))
   }
-  
+
   console.log()
 }
