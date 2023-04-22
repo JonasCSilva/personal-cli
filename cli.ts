16342e9ab13e686826dba3f9f8b757df3bcdb92c
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2023-04-22 01:07:16 -0300

    feat: remove 'zip-onedrive' command

--- cli.ts
+++ cli.ts
@@ -1,7 +1,6 @@
 import { Command } from 'https://deno.land/x/cliffy@v0.25.7/command/mod.ts'
 import { checkFolders } from './check-folders/index.ts'
 import { checkRepositories } from './check-repositories/index.ts'
-import { zipOneDrive } from './zip-onedrive/7zip.ts'
 
 await new Command()
   .name('pc')
@@ -21,9 +20,5 @@ await new Command()
   .command('cr', 'Check repositories.')
   .arguments('[path:string]')
   .action(async (_options, arg) => await checkRepositories(arg))
-  // Child command 3
-  .command('zo', 'Zip OneDrive folder.')
-  .arguments('[path:string]')
-  .action(async (_options, arg) => await zipOneDrive(arg))
   // Parse
   .parse()
