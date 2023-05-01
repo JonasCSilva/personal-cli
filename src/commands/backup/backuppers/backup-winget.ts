ba255fc4284e82342e868a5fcb0c1d7c0290f32a
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2023-05-01 17:30:40 -0300

    feat: improve winget backupper

--- src/commands/backup/backuppers/backup-winget.ts
+++ src/commands/backup/backuppers/backup-winget.ts
@@ -11,15 +11,55 @@ export default async function backup(path: string): Promise<void> {
 
   const outStr = decoder.decode(stdout)
 
-  const array = outStr.split('\r\n').filter((_) => _ && !_.includes('-----------')).map((_) =>
-    _.replaceAll('…', '… ').split(/\s{2,}/g).filter((_) => _ && !_.includes('\b'))
-  )
+  const array = outStr.split('\r\n').filter((_) => _ && !_.includes('-----------'))
 
-  const data = array.slice(1).map((_) => {
-    const object: { [key: string]: string } = {}
-    for (const [index, entry] of _.entries()) {
-      object[array[0][index]] = entry
+  const rawIndexes = array.map((_) => [_.indexOf('…'), _.lastIndexOf('…')])
+
+  const indexes: number[] = []
+
+  for (const [first, last] of rawIndexes) {
+    if (first !== -1 && !indexes.includes(first)) indexes.push(first)
+    if (last !== -1 && !indexes.includes(last)) indexes.push(last)
+  }
+
+  indexes.sort()
+
+  const arrays = array.map((_) => {
+    const first = _.substring(0, indexes[0] + 1).trim()
+
+    const second = _.substring(indexes[0] + 2, indexes[1] + 1).trim()
+
+    const rawThirdAndFourth = _.substring(indexes[1] + 2).trim()
+    const thirdAndFourth = rawThirdAndFourth.split(/\s{2,}/g)
+
+    const third = thirdAndFourth[0]
+    const fourth = thirdAndFourth[1]
+
+    const rawStrings = [first, second, third, fourth]
+
+    const strings = []
+
+    for (const rawString of rawStrings) {
+      if (rawString) {
+        strings.push(rawString)
+      }
     }
+
+    return strings
+  })
+
+  const header = arrays[0]
+  header[0] = header[0].match(/[A-Z].+/)![0]
+
+  const main = arrays.slice(1)
+
+  const data = main.map((_) => {
+    const object: { [key: string]: string } = {}
+
+    for (const [index, entry] of _.entries()) {
+      object[header[index]] = entry
+    }
+
     return object
   })
 
