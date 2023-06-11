f0b2e77baf5313e98fd746e551774f3880801fe1
JonasCSilva <55038425+JonasCSilva@users.noreply.github.com>
2023-06-10 23:12:39 -0300

    feat: add spotify backupper

---
+++ src/commands/backup/backuppers/backup-spotify/auth.ts
@@ -0,0 +1,68 @@
+import { Application, Router } from 'oak'
+import { encode } from 'encoding/base64.ts'
+import { getEnvValue } from 'utils/get-env-value.ts'
+
+const SPOTIFY_CLIENT_ID = await getEnvValue('SPOTIFY_CLIENT_ID')
+const SPOTIFY_CLIENT_SECRET = await getEnvValue('SPOTIFY_CLIENT_SECRET')
+
+const REDIRECT_URI = 'http://localhost:8080/callback'
+
+const SCOPES = [
+  'playlist-read-private',
+  'user-library-read',
+].join(' ')
+
+const url = 'https://accounts.spotify.com/authorize?response_type=code' +
+  `&client_id=${encodeURIComponent(SPOTIFY_CLIENT_ID)}` +
+  `&scope=${encodeURIComponent(SCOPES)}` +
+  `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`
+
+const app = new Application()
+const router = new Router()
+
+router
+  .get('/callback', async (ctx) => {
+    const code = ctx.request.url.searchParams.get('code')
+
+    if (!code) {
+      console.log(`No code provided`)
+      ctx.response.body = `${ctx.request.body}`
+      return
+    }
+
+    const token = encode(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`)
+
+    const urlParams = new URLSearchParams({
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: REDIRECT_URI,
+    })
+
+    const data = await fetch(
+      'https://accounts.spotify.com/api/token',
+      {
+        method: 'POST',
+        body: urlParams,
+        headers: {
+          'Authorization': `Basic ${token}`,
+        },
+      },
+    )
+
+    const { access_token, refresh_token } = await data.json()
+
+    console.log(
+      `ACCESS TOKEN: ${access_token}\n\n` +
+        `REFRESH TOKEN: ${refresh_token}\n\n`,
+    )
+
+    ctx.response.body = `You can close this window now.`
+  })
+
+app.use(router.routes())
+
+app.addEventListener('listen', () => {
+  console.log(`To get OAuth tokens, navigate to:\n${url}`)
+})
+
+await app.listen({ port: 8080 })
--- src/commands/backup/index.ts
+++ src/commands/backup/index.ts
@@ -2,12 +2,14 @@ import backupGitHub from './backuppers/backup-github.ts'
 import backupHabitica from './backuppers/backup-habitica.ts'
 import backupScoop from './backuppers/backup-scoop.ts'
 import backupWinget from './backuppers/backup-winget.ts'
+import backupSpotify from './backuppers/backup-spotify/index.ts'
 import { getBackupPath } from '../../utils/getPaths.ts'
 
 export async function backup() {
   const backupPath = await getBackupPath()
 
   await Promise.all([
+    backupSpotify(backupPath),
     backupGitHub(backupPath),
     backupHabitica(backupPath),
     backupScoop(backupPath),
